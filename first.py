4b3273f2396f98ce3bfb6be581673d2fcd07e75d
petr-03 <petr.roth1@gmail.com>
2025-10-30 18:32:16 +0100

    Add function to determine if a number is even or odd

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print ("Ahoj")
+def sudy_nebo_lichy(X):
+    if X % 2 == 0:
+        číslo = "sudé"
+    else:
+        číslo = "liché"
+
+    return f"Číslo {X} je {číslo}" 
+
+print(sudy_nebo_lichy(5))
+print(sudy_nebo_lichy(1000000))
